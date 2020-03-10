2f0043da7a445bd64d3b710c493b27f9930201be
mihai <mihaiandrei_99@yahoo.com>
2020-03-10 14:16:00 +0200

    added modal

---
+++ javascript/modal-handler.js
@@ -0,0 +1,8 @@
+function openBackground() {
+    let el = document.getElementById('modal-background');
+    el.style.display = 'block';
+}
+function closeBackground() {
+    let el = document.getElementById('modal-background');
+    el.style.display = 'none';
+}
