1a37e50413a6461719dfff26910d919e409c08e3
mihai <mihaiandrei_99@yahoo.com>
2020-03-10 13:53:51 +0200

    added logo

--- javascript/page-loaders.js
+++ javascript/page-loaders.js
@@ -1,5 +1,2 @@
-function loadHomepage() {
-    document.getElementById("content").src="./pages/homepage.html";
-}
 
 function adjustContentHeight() { document.getElementById("content").style.height = document.getElementById("content").contentWindow.document.body.scrollHeight + "px"; }
