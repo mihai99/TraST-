4529c60add56d5b144e2fa7c93ac04fbac03c2b0
Ciubi <vlad.ciubotariuvlad99@students.info.uaic.ro>
2020-05-03 15:34:53 +0300

    Codul penal - complet

---
+++ pages/invatare/cap1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../../styles/general.css">
+        <link rel="stylesheet" type="text/css" href="../../styles/invatare/legislatie.css">
+    </head>
+
+    <body>
+        <div class="body-wrapper">
+            <div class="cap-nav">
+                <a id="dummy"></a>
+                <a href="../m_invatare.html" id="home">CAPITOLUL I: Dispoziții generale</a>
+                <a href="cap2.php" id="next">Capitolul 2</a>
+            </div>
+            <?php
+            include "cap1.html";
+            ?>
+        </div>
+    </body>
+
+</html>
